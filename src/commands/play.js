b7fc73e1f38c6878d0550a8dfe31239b56e169d2
Feroconas <rafatinoni@gmail.com>
2024-12-05 01:37:44 +0000

    Renato bangers

--- src/commands/play.js
+++ src/commands/play.js
@@ -2,7 +2,7 @@ import { ChatInputCommandInteraction, InteractionContextType, SlashCommandBuilde
 import { AudioManagerError, getAudioManager, getInteractionUserVoiceChannel, joinVoiceChannel } from '../voice-manager.js';
 import { getVoiceConnection } from '@discordjs/voice';
 
-export default { // TODO Add 'Youtube search term or' to description and replies in the future
+export default {
     data: new SlashCommandBuilder()
         .setName('play')
         .setDescription('Plays an audio or adds it to the queue.')
@@ -15,28 +15,32 @@ export default { // TODO Add 'Youtube search term or' to description and replies
     * @param {ChatInputCommandInteraction} interaction
     */
     async execute(interaction) { // TODO Check permission
-        const guildId = interaction.guildId;
-        const audioManager = getAudioManager(guildId);
-        const voiceConnection = getVoiceConnection(guildId);
-        const userVoiceChannel = await getInteractionUserVoiceChannel(interaction);
-        if (!voiceConnection && !userVoiceChannel) {
-            await interaction.reply({ content: 'Either you or I must be in a voice channel.', ephemeral: true });
-            return;
-        }
-        const query = interaction.options.getString('query');
-        let enqueuedAudioURL;
-        try {
-            enqueuedAudioURL = await audioManager.enqueueAudio(query);
-        } catch (error) {
-            if (error instanceof AudioManagerError) {
-                await interaction.reply({ content: `${error.message}`, ephemeral: true });
-                return;
-            }
-            throw new Error();
-        }
-        if (!voiceConnection)
-            joinVoiceChannel(userVoiceChannel);
-        const startedPlaying = audioManager.play();
-        await interaction.reply(startedPlaying ? `Playing ${enqueuedAudioURL}.` : `Added ${enqueuedAudioURL} to the queue.`);
+        await play(interaction);
     },
 };
+
+export async function play(interaction, queryOverride) {
+    const guildId = interaction.guildId;
+    const audioManager = getAudioManager(guildId);
+    const voiceConnection = getVoiceConnection(guildId);
+    const userVoiceChannel = await getInteractionUserVoiceChannel(interaction);
+    if (!voiceConnection && !userVoiceChannel) {
+        await interaction.reply({ content: 'Either you or I must be in a voice channel.', ephemeral: true });
+        return;
+    }
+    const query = queryOverride ? queryOverride : interaction.options.getString('query');
+    let enqueuedAudioURL;
+    try {
+        enqueuedAudioURL = await audioManager.enqueueAudio(query);
+    } catch (error) {
+        if (error instanceof AudioManagerError) {
+            await interaction.reply({ content: `${error.message}`, ephemeral: true });
+            return;
+        }
+        throw new Error();
+    }
+    if (!voiceConnection)
+        joinVoiceChannel(userVoiceChannel);
+    const startedPlaying = audioManager.play();
+    await interaction.reply(startedPlaying ? `Playing ${enqueuedAudioURL}.` : `Added ${enqueuedAudioURL} to the queue.`);
+}
