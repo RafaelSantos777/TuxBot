ca21c5698a536cc417337362d56e7e7360cfed8b
Feroconas <rafatinoni@gmail.com>
2025-07-31 21:48:40 +0100

    Refactor: update command description

--- src/commands/dick.ts
+++ src/commands/dick.ts
@@ -3,58 +3,58 @@ import EMOJIS from '../../data/emojis.json' with { type: 'json' };
 import { Command, CommandContext } from '../types/command.js';
 import { getUserFromContext } from '../command.js';
 
-const BASE_DICK_SIZE_CENTIMETERS = 13.75;
-const MAX_EXTRA_DICK_SIZE_CENTIMETERS = 0.5;
+const BASE_STICK_SIZE_CENTIMETERS = 13.75;
+const MAX_EXTRA_STICK_SIZE_CENTIMETERS = 0.5;
 const MAX_BASE_MULTIPLER = 2;
 const MULTIPLIER_EXPONENT = 3;
 const MULTIPLIER_CHANCE = 0.15;
-const HUGE_DICK_SIZE_CENTIMETERS = 30;
-const BIG_DICK_SIZE_CENTIMETERS = 18;
-const SMALL_DICK_SIZE_CENTIMETERS = 10;
-const TINY_DICK_SIZE_CENTIMETERS = 5;
+const HUGE_STICK_SIZE_CENTIMETERS = 30;
+const BIG_STICK_SIZE_CENTIMETERS = 18;
+const SMALL_STICK_SIZE_CENTIMETERS = 10;
+const TINY_STICK_SIZE_CENTIMETERS = 5;
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('dick')
-		.setDescription('Measures your dick.'),
+		.setName('stick')
+		.setDescription('Measures your stick.'),
 	async execute(context: CommandContext) {
 		const user = getUserFromContext(context);
-		await context.reply(`${createDickReply(user.displayName)}`);
+		await context.reply(`${createStickReply(user.displayName)}`);
 	},
 } as Command;
 
-function createDickReply(userDisplayName: string): string {
+function createStickReply(userDisplayName: string): string {
 
-	function generateRandomDickSizeCentimeters(): number {
-		const base_size = Math.random() * BASE_DICK_SIZE_CENTIMETERS + Math.random() * BASE_DICK_SIZE_CENTIMETERS;
-		const extra_size = Math.random() * MAX_EXTRA_DICK_SIZE_CENTIMETERS;
+	function generateRandomStickSizeCentimeters(): number {
+		const base_size = Math.random() * BASE_STICK_SIZE_CENTIMETERS + Math.random() * BASE_STICK_SIZE_CENTIMETERS;
+		const extra_size = Math.random() * MAX_EXTRA_STICK_SIZE_CENTIMETERS;
 		const multiplier = Math.random() < MULTIPLIER_CHANCE ? Math.pow(Math.random() * MAX_BASE_MULTIPLER, MULTIPLIER_EXPONENT) : 1;
 		return base_size * multiplier + extra_size;
 	}
 
-	function formatDickSizeWithUnits(): string {
-		return dickSizeCentimeters >= 100
-			? `${(dickSizeCentimeters / 100).toFixed(2)} m`
-			: `${dickSizeCentimeters.toFixed(1)} cm`;
+	function formatStickSizeWithUnits(): string {
+		return stickSizeCentimeters >= 100
+			? `${(stickSizeCentimeters / 100).toFixed(2)} m`
+			: `${stickSizeCentimeters.toFixed(1)} cm`;
 	}
 
-	function createDickShape(): string {
-		return `8${'='.repeat(dickSizeCentimeters / 2)}D`;
+	function createStickShape(): string {
+		return `8${'='.repeat(stickSizeCentimeters / 2)}D`;
 	}
 
 	function getEmoji(): string | null {
-		if (dickSizeCentimeters >= HUGE_DICK_SIZE_CENTIMETERS)
+		if (stickSizeCentimeters >= HUGE_STICK_SIZE_CENTIMETERS)
 			return EMOJIS.MONKAW;
-		else if (dickSizeCentimeters >= BIG_DICK_SIZE_CENTIMETERS)
+		else if (stickSizeCentimeters >= BIG_STICK_SIZE_CENTIMETERS)
 			return EMOJIS.POG;
-		else if (dickSizeCentimeters <= TINY_DICK_SIZE_CENTIMETERS)
+		else if (stickSizeCentimeters <= TINY_STICK_SIZE_CENTIMETERS)
 			return EMOJIS.OMEGALUL;
-		else if (dickSizeCentimeters <= SMALL_DICK_SIZE_CENTIMETERS)
+		else if (stickSizeCentimeters <= SMALL_STICK_SIZE_CENTIMETERS)
 			return EMOJIS.KEKW;
 		return null;
 	}
 
-	const dickSizeCentimeters = generateRandomDickSizeCentimeters();
+	const stickSizeCentimeters = generateRandomStickSizeCentimeters();
 	const emoji = getEmoji();
-	return `${userDisplayName}'s dick is ${bold(formatDickSizeWithUnits())} long.${emoji ? ' ' + emoji : ''}\n${bold(createDickShape())}`;
+	return `${userDisplayName}'s stick is ${bold(formatStickSizeWithUnits())} long.${emoji ? ' ' + emoji : ''}\n${bold(createStickShape())}`;
 }
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ async function main() {
     } catch (error) {
         console.error(error);
         await logout();
-        process.exit(1);
     }
 }
 
