0b0d4b78a6cd7ad51a31e9dac6bbb2352f78ef41
Feroconas <rafatinoni@gmail.com>
2026-01-31 12:39:47 +0000

    Simplify client

--- src/client.ts
+++ src/client.ts
@@ -1,53 +1,29 @@
-import { Client, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
-import fs from 'fs';
-import path from 'path';
-import { pathToFileURL } from 'url';
-import { Command } from './types/command.js';
-import { executeCommand } from './command.js';
+import { executeCommand, setupCommands } from './command.js';
 import { addTrackManager } from './track-manager.js';
 import { disconnectIfAlone } from './voice.js';
 
-const COMMAND_FOLDER_PATH = path.join(import.meta.dirname, 'commands');
-const { BOT_TOKEN, APPLICATION_ID } = process.env as { BOT_TOKEN: string; APPLICATION_ID: string; };
+
+export const { BOT_TOKEN, APPLICATION_ID } = process.env as { BOT_TOKEN: string; APPLICATION_ID: string; };
 const CLIENT_INTENTS = [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent];
 export const client = new Client({ intents: CLIENT_INTENTS });
-export const commandMap: Map<string, Command> = new Map();
+
+function setupEventHandlers() {
+    client.on(Events.InteractionCreate, (interaction) => {
+        if (interaction.isChatInputCommand())
+            executeCommand(interaction);
+    });
+    client.on(Events.MessageCreate, (message) => {
+        if (message.inGuild() && !message.author.bot)
+            executeCommand(message);
+    });
+    client.on(Events.VoiceStateUpdate, disconnectIfAlone);
+    client.on(Events.GuildCreate, addTrackManager);
+}
 
 export async function setupClient() {
-
-    async function setupCommands() {
-        for (const fileName of fs.readdirSync(COMMAND_FOLDER_PATH)) {
-            const fileURL = pathToFileURL(path.join(COMMAND_FOLDER_PATH, fileName));
-            const command = (await import(fileURL.href)).default as Command;
-            commandsData.push(command.data);
-            commandMap.set(command.data.name, command);
-            for (const alias of command.aliases ?? [])
-                commandMap.set(alias, command);
-        }
-    }
-
-    async function deployCommands() {
-        const rest = new REST().setToken(BOT_TOKEN);
-        await rest.put(Routes.applicationCommands(APPLICATION_ID), { body: commandsData });
-    }
-
-    function setupEventHandlers() {
-        client.on(Events.InteractionCreate, (interaction) => {
-            if (interaction.isChatInputCommand())
-                executeCommand(interaction);
-        });
-        client.on(Events.MessageCreate, (message) => {
-            if (message.inGuild() && !message.author.bot)
-                executeCommand(message);
-        });
-        client.on(Events.VoiceStateUpdate, disconnectIfAlone);
-        client.on(Events.GuildCreate, addTrackManager);
-    }
-
-    const commandsData: SlashCommandBuilder[] = [];
     await setupCommands();
-    await deployCommands();
     setupEventHandlers();
 }
 
--- src/command.ts
+++ src/command.ts
@@ -1,12 +1,40 @@
-import { ChatInputCommandInteraction, Message } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+import { pathToFileURL } from 'url';
+import { ChatInputCommandInteraction, Message, REST, Routes, SlashCommandBuilder } from 'discord.js';
 import { extractCommandName } from './prefix-manager.js';
-import { commandMap } from './client.js';
-import { CommandContext } from './types/command.js';
+import { Command, CommandContext } from './types/command.js';
+import { APPLICATION_ID, BOT_TOKEN } from './client.js';
 
+export const commandMap: Map<string, Command> = new Map();
+const COMMAND_FOLDER_PATH = path.join(import.meta.dirname, 'commands');
 const COMMAND_ERROR_REPLY_OPTIONS = { content: 'An unexpected error occurred while executing this command! ⚠️', ephemeral: true };
 
 export function getUserFromContext(context: CommandContext) {
     return context instanceof Message ? context.author : context.user;
+}
+
+export async function setupCommands() {
+
+    async function setupCommandMap() {
+        for (const fileName of fs.readdirSync(COMMAND_FOLDER_PATH)) {
+            const fileURL = pathToFileURL(path.join(COMMAND_FOLDER_PATH, fileName));
+            const command = (await import(fileURL.href)).default as Command;
+            commandsData.push(command.data);
+            commandMap.set(command.data.name, command);
+            for (const alias of command.aliases ?? [])
+                commandMap.set(alias, command);
+        }
+    }
+
+    async function deployCommands() {
+        const rest = new REST().setToken(BOT_TOKEN);
+        await rest.put(Routes.applicationCommands(APPLICATION_ID), { body: commandsData });
+    }
+
+    const commandsData: SlashCommandBuilder[] = [];
+    await setupCommandMap();
+    await deployCommands();
 }
 
 export async function executeCommand(context: CommandContext) {
